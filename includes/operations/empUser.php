3b4d23456c39a911c12604f32f0942ca818490b3
joelbh92 <joelbh92@gmail.com>
2024-06-09 18:36:47 +0000

    update empleado

--- includes/operations/empUser.php
+++ includes/operations/empUser.php
@@ -87,8 +87,8 @@ if(!empty($_SESSION['usNamePlataform'])){
     
     //verificamos que los campos esten capturados
     if(!empty($nombreUpdate) || !empty($paternoUpdate) || !empty($maternoUpdate) ||
-      !empty($correoUpdate) || !empty($celularUpdate) || !empty($departamentoUpdate) 
-      || !empty($activoUpdate)){
+      !empty($correoUpdate) || !empty($celularUpdate) || !empty($departamentoUpdate) ||
+      !empty($activoUpdate)){
       //procedemos a actualizar los datos del usuario
       $sql = "UPDATE empleados SET nombre = '$nombreUpdate', paterno = '$paternoUpdate',
       materno = '$maternoUpdate', correo = '$correoUpdate', celular = '$celularUpdate',
